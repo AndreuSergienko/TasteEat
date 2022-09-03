d9bbdb453d8b8aa56faa61d4796d21d91b283b3a
Andrey <kolsergienko@gmail.com>
2022-09-03 22:14:50 +0300

    adding burger menu and adaptive

--- src/components/Header/header.js
+++ src/components/Header/header.js
@@ -1,1 +1,33 @@
-import "./header.scss";
+import './header.scss'
+
+const burgerButton = document.querySelector('.header__burger')
+const burgerMenu = document.querySelector('.header__burger-menu')
+const menuClose = burgerMenu.querySelector('.header__burger-close')
+const overlay = document.querySelector('.overlay')
+
+burgerButton.addEventListener('click', (e) => {
+	const target = e.target.closest('.header__burger')
+
+	target.classList.add('hide')
+
+	burgerMenu.classList.add('active')
+	overlay.classList.add('active')
+
+	if (menuClose) {
+		menuClose.addEventListener('click', (e) => {
+			burgerMenu.classList.remove('active')
+			target.classList.remove('hide')
+			overlay.classList.remove('active')
+		})
+	}
+
+	if (overlay) {
+		overlay.addEventListener('click', (e) => {
+			const target = e.target.closest('.overlay')
+
+			target.classList.remove('active')
+			burgerMenu.classList.remove('active')
+			burgerButton.classList.remove('hide')
+		})
+	}
+})

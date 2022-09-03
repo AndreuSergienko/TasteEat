f46225830ca7abdbae86ccef7267c9d418744809
Andrey <kolsergienko@gmail.com>
2022-09-03 19:33:44 +0300

    export icons and add header

--- src/index.js
+++ src/index.js
@@ -1,11 +1,9 @@
-import "./main.scss";
+import './main.scss'
 
 //Import Layout
 
 import './layout/main/main.scss'
 
 //Import components
-import "./components/header/header";
-import "./components/footer/footer";
-import './components/searchForm/searchForm';
-import './components/common/dropdown/dropdown'
+import './components/Header/header'
+import './components/footer/footer'

4bdc28a5f2dd32a534e1c96836178de3f363fe68
Andrew <kolsergienko@gmail.com>
2022-09-06 19:18:33 +0300

    adding story section and logic for burgerButton

--- src/components/Header/header.js
+++ src/components/Header/header.js
@@ -2,8 +2,18 @@ import './header.scss'
 
 const burgerButton = document.querySelector('.header__burger')
 const burgerMenu = document.querySelector('.header__burger-menu')
-const menuClose = burgerMenu.querySelector('.header__burger-close')
 const overlay = document.querySelector('.overlay')
+const body = document.querySelector('body')
+
+window.addEventListener('scroll', () => {
+	let scrollY = window.pageYOffset
+
+	if (scrollY > 100) {
+		burgerButton.classList.add('scrolled')
+	} else {
+		burgerButton.classList.remove('scrolled')
+	}
+})
 
 burgerButton.addEventListener('click', (e) => {
 	const target = e.target.closest('.header__burger')
@@ -12,32 +22,72 @@ burgerButton.addEventListener('click', (e) => {
 
 	burgerMenu.classList.add('active')
 	overlay.classList.add('active')
+	body.classList.add('lock')
 
-	if (menuClose) {
-		menuClose.addEventListener('click', (e) => {
-			burgerMenu.classList.remove('active')
-			target.classList.remove('hide')
-			overlay.classList.remove('active')
-		})
-	}
+})
 
-	if (overlay) {
-		overlay.addEventListener('click', (e) => {
-			const target = e.target.closest('.overlay')
+const closingElements = document.querySelectorAll('[data-closing]')
 
-			target.classList.remove('active')
-			burgerMenu.classList.remove('active')
-			burgerButton.classList.remove('hide')
-		})
-	}
-
-	const burgerLinks = burgerMenu.querySelectorAll('.header__burger-link')
-
-	burgerLinks.forEach(link => {
-		link.addEventListener('click', (e) => {
-			burgerMenu.classList.remove('active')
-			overlay.classList.remove('active')
-			burgerButton.classList.remove('hide')
-		})
+closingElements.forEach(el => {
+	el.addEventListener('click', (e) => {
+		e.preventDefault()
+		burgerButton.classList.remove('hide')
+		burgerMenu.classList.remove('active')
+		overlay.classList.remove('active')
+		body.classList.remove('lock')
 	})
 })
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// if (menuClose) {
+	// 	menuClose.addEventListener('click', (e) => {
+	// 		burgerMenu.classList.remove('active')
+	// 		target.classList.remove('hide')
+	// 		overlay.classList.remove('active')
+	// 		body.classList.remove('lock')
+	// 	})
+	// }
+
+	// if (overlay) {
+	// 	overlay.addEventListener('click', (e) => {
+	// 		const target = e.target.closest('.overlay')
+	// 		target.classList.remove('active')
+	// 		burgerMenu.classList.remove('active')
+	// 		burgerButton.classList.remove('hide')
+	// 		body.classList.remove('lock')
+	// 	})
+	// }
+
+	// const burgerLinks = burgerMenu.querySelectorAll('.header__burger-link')
+
+	// burgerLinks.forEach(link => {
+	// 	link.addEventListener('click', (e) => {
+	// 		burgerMenu.classList.remove('active')
+	// 		overlay.classList.remove('active')
+	// 		burgerButton.classList.remove('hide')
+	// 	})
+	// })
--- src/index.js
+++ src/index.js
@@ -11,3 +11,4 @@ import './components/footer/footer'
 // Import index page components
 import './components/pages/index/hero/hero'
 import './components/pages/index/contacts/contacts'
+import './components/pages/index/story/story'

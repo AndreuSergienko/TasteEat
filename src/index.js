4a98ae78ec0a1d9d6b108f3fc49b2515a5cdde83
Andrew <kolsergienko@gmail.com>
2022-09-12 21:23:43 +0300

    adding testimonial section and init swiper : )

--- src/index.js
+++ src/index.js
@@ -13,3 +13,38 @@ import './components/pages/index/hero/hero'
 import './components/pages/index/contacts/contacts'
 import './components/pages/index/story/story'
 import './components/pages/index/special/special'
+import './components/pages/index/testimonial/testimonial'
+
+// Swiper initialization
+import Swiper, { Navigation, Pagination } from 'swiper';
+// import Swiper and modules styles
+import 'swiper/css';
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';
+
+const swiper = new Swiper(".mySwiper", {
+   modules: [Navigation, Pagination],
+   loop: true,
+   grabCursor: true,
+   pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+   },
+   navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+   },
+   breakpoints: {
+      1200: {
+         slidesPerView: 2.5,
+         spaceBetween: 30,
+      },
+      768: {
+         slidesPerView: 2,
+         spaceBetween: 20,
+      },
+      320: {
+         slidesPerView: 1,
+      }
+   }
+});
